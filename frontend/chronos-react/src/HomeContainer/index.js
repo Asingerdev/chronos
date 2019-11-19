61e05eae40bc7cf976eae845a3c21570063a7179
Combatd <combatdstar@gmail.com>
2019-11-19 13:31:01 -0800

    create HomeContainer component

---
+++ frontend/chronos-react/src/HomeContainer/index.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+
+class HomeContainer extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            timelines: [],
+            loggedUser: false
+        }
+
+       
+    }
+
+    componentDidMount() {
+        this.getTimelines();
+    }
+    getTimelines = async () => {
+
+        try {
+        
+            const timelines = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/timelines`, {
+                credentials: 'include',
+                method: "Get"
+            });
+
+            const parsedTimelines = await timelines.json();
+            console.log(parsedTimelines);
+            
+            this.setState({
+                // MAY need to change data property here
+                timelines: parsedTimelines.data
+            });
+ 
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+    addTimeline = async (e, timeline) => {
+        e.preventDefault(); 
+        console.log(timeline);
+
+        try {
+            // createdTimelineResponse stores response from Flask API
+            const createdTimelineResponse = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/timelines`, {
+                method: 'POST',
+                credentials: 'include',
+                body: JSON.stringify(timeline),
+                headers: {
+                    'content-type': 'application/json'
+                }
+            });
+
+            // convert Flask response into object we can use
+            const parsedResponse = await createdTimelineResponse.json();
+            console.log(parsedResponse, ' <= parsedResponse');
+
+            this.setState({
+                timelines: [...this.state.timelines, parsedResponse.data]
+            });
+
+
+        } catch (err) {
+            console.log('error');
+            console.log(err);
+        }
+
+    }
+    deleteTimeline = async (id) => {
+
+        console.log(id);
+        const deleteTimelineResponse = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/timelines`, {
+                                                            method: 'DELETE',
+                                                            credentials: 'include'
+        });
+
+        // parsed response from timeline
+        const deleteTimelineParsed = await deleteTimelineResponse.json();
+        console.log(deleteTimelineResponse);
+
+        // after removing from db, delete specific timeline from state
+        this.setState({
+            timelines: this.state.timelines.filter( (timeline) => timeline.id != id )
+        });
+
+        console.log(deleteTimelineParsed, ' <= response from Flask server');
+    }
+
+
+}
