b734273e8ac876021b3e77eb9e975a6abd9157e6
Andrew Singer <andrew.singer7@gmail.com>
2019-11-22 11:37:54 -0800

    Create add timeline button

--- frontend/chronos-react/src/Components/TimeContainer/index.js
+++ frontend/chronos-react/src/Components/TimeContainer/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import TimeList from '../TimeList'
 import AddModal from '../AddModal'
 
-
+import Button from './style'
 
 
 class TimeContainer extends Component {
@@ -12,7 +12,7 @@ class TimeContainer extends Component {
         this.state = {
             timelines: [],
             loggedUser: false,
-            showModal: true
+            showModal: null
         }
     }
     componentDidMount() {
@@ -166,8 +166,8 @@ class TimeContainer extends Component {
                        :
                     null
                }
+               <Button onClick={this.showModal}>+ Add Timeline</Button>
                <TimeList timelines={this.state.timelines} />
-                
             </React.Fragment>
         )
     }
