c5dfea6299a7f1c4558d6f5769e697f1d104361c
Combatd <combatdstar@gmail.com>
2019-11-25 12:13:52 -0800

    show page is able to make api call to backend get_one_timeline(id)

--- frontend/chronos-react/src/Components/TimeShow/index.js
+++ frontend/chronos-react/src/Components/TimeShow/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AddEventModal from '../AddEventModal';
 import EditEventModal from '../EditEventModal';
@@ -9,18 +9,50 @@ import StyleButton from './stylebutton';
 // <Route exact path="/timelines/:id" render={(props) => <TimeShow {...props} />}/
 
 // Each timeline has an array of events
-const TimeShow = (props) => {
-    console.log(props, " the props")
-    console.log(props.location)
+class TimeShow extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            timeline : {}
+        }
+    }
+
+
+    
+
+    getTimeline = async () => {
+        const timelineId = this.props.match.params.id;
+        try {
+            const timeline = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/timelines/${timelineId}`, {
+                credentials: 'include',
+                method: "Get",
+                "Content-Type": "application/json"
+            });
+            const parsedTimeline = await timeline.json();
+            this.setState({
+                timeline: parsedTimeline.data
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+ 
+    componentDidMount() {
+        this.getTimeline()
+    }
+
+    
+    // console.log(props.location)
     // const timelineId = props.match.params.id
-    const timelineId = props.match.params.id
-    console.log(timelineId, " props.match.params.id")
+    // const timelineId = props.match.params.id
+    // console.log(timelineId, " props.match.params.id")
     
     // May need to pass props from TimeList
     // We need to pass down the props to get the modals to show and hide
 
-    const theTimeline = props.timeline;
-    console.log(theTimeline, " all of the timelines");
+    // const theTimeline = props.timeline;
+    // console.log(theTimeline, " all of the timelines");
     
 
     // const foundTimeline = allTimelines.filter( timeline => timeline.id !== timeline.timelineId );
@@ -34,18 +66,17 @@ const TimeShow = (props) => {
 
     // We will need to idiomatically pass down props from HomeContainer, to TimeList,
     // all the way down to this TimeShow.
-
-    return (
+    render() {
+        return (
         <React.Fragment>
-            {/* {eventsList}
-
-            <button onClick={() => props.openAndEdit(foundTimeline)} >Edit Timeline</button>
-            button
-            <button onClick={() => props.deleteTimeline(foundTimeline.timeline_id)} >Delete Timeline</button>
-            {eventsList}  */}
-
+            <h1>{this.state.timeline.title}</h1>
+        <h2>Date From: {this.state.timeline.date_from}</h2>
+        <h2>Date To: {this.state.timeline.date_to}</h2>
+        <img src={this.state.timeline.thumbnail}></img>
         </React.Fragment>
-    )
+        )
+    }
+   
 
 
 };
