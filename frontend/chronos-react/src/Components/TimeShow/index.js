31f4820d4fbdfb0be91caf9f90560f06945a5fd6
Combatd <combatdstar@gmail.com>
2019-11-25 11:51:41 -0800

    found out that TimeShow needs to be stateful to GET  the get_one_timeline route"

--- frontend/chronos-react/src/Components/TimeShow/index.js
+++ frontend/chronos-react/src/Components/TimeShow/index.js
@@ -11,45 +11,26 @@ import StyleButton from './stylebutton';
 // Each timeline has an array of events
 const TimeShow = (props) => {
     console.log(props, " the props")
+    console.log(props.location)
     // const timelineId = props.match.params.id
-    const timelineId = props.id
-    console.log(timelineId, " props.id")
+    const timelineId = props.match.params.id
+    console.log(timelineId, " props.match.params.id")
     
     // May need to pass props from TimeList
     // We need to pass down the props to get the modals to show and hide
 
-    const allTimelines = props.timelines;
-    console.log(allTimelines, " all of the timelines");
+    const theTimeline = props.timeline;
+    console.log(theTimeline, " all of the timelines");
     
 
-    const foundTimeline = allTimelines.filter( timeline => timeline.id !== timeline.timelineId );
-    console.log(foundTimeline, " this is the foundTimeline");
+    // const foundTimeline = allTimelines.filter( timeline => timeline.id !== timeline.timelineId );
+    // console.log(foundTimeline, " this is the foundTimeline");
     // const selectedTimeline = foundTimeline[0];
     // console.log(selectedTimeline, " <= get the 0 index timeline");
     
     
 
-    // const eventsList = selectedTimeline.events.map((event, i) => {
-
-
-    //     return (
-    //         <div>
-    //             <ShowDiv>
-    //                 <div key={i} >
-                        
-    //                     <Image src={event.event_thumbnail} alt={event.event_name} />
-    //                     <h3>{event.event_date}</h3>
-    //                     <p>{event.event_desc}</p>
-    //                     <Image src={event.event_thumbnail} />
-    //                 </div>
-    //             </ShowDiv>    
-    //         </div>
-    //     )
-
-
-
-
-    // }); // end foundTimeline.events.map
+    // const eventsList = selectedTimeline.events;
 
     // We will need to idiomatically pass down props from HomeContainer, to TimeList,
     // all the way down to this TimeShow.
